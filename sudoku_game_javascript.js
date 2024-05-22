ce4fdb668fb5839af9ad71ae337079be1dde9ff9
dineshkr01 <aec.cse.dinesh@gmail.com>
2024-05-22 20:32:56 +0530

    Delete sudoku_game_javascript.js

--- sudoku_game_javascript.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("div3").addEventListener("click", function () {
-  const selectedOption = document.getElementById('difficultyDropdown').value;
-  window.location.href = `sudoku_main_game_page.html?selectedOption=${selectedOption}`;
-});
